14df7f2ac9bf78309bc5a59034148b02cd5fde34
Papadritta <90826754+papadritta@users.noreply.github.com>
2022-11-01 23:45:34 +0700

    activate disable service
    need when you run the second time script

--- box/run.sh
+++ box/run.sh
@@ -41,7 +41,7 @@ rustup target add wasm32-unknown-unknown --toolchain nightly
 
 printCyan "Deleting Node previous installation..." && sleep 1
 sudo systemctl stop gear-node
-#sudo systemctl disable gear-node
+sudo systemctl disable gear-node
 sudo rm -rf /root/.local/share/gear-node
 sudo rm /etc/systemd/system/gear-node.service
 sudo rm /root/gear
