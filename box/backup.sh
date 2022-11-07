cf6a9dd10adbf955d4fefac7cd64589ebc12a047
Papadritta <90826754+papadritta@users.noreply.github.com>
2022-11-07 11:14:57 +0700

    update sh
    add&edit code line

--- box/backup.sh
+++ box/backup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+source <(curl -s https://raw.githubusercontent.com/papadritta/scripts/main/main.sh)
+
+printLogo
+
+printCyan "Updating packages..." && sleep 1
+sudo apt update && sudo apt upgrade -y
+
+printCyan "Copy Key to backup..." && sleep 1
 cd $HOME
 sudo mkdir /root/backup
 chmod +x /root/backup
@@ -6,4 +14,9 @@ sudo systemctl stop gear-node
 cd /root/.local/share/gear-node/chains
 sudo cp gear_staging_testnet_v3/network/secret_ed25519 /root/backup
 cd $HOME
-sudo systemctl start gear-node
+sudo systemctl restart gear-node
+
+printLine
+
+printCyan "your key stored /root/backup/secret_ed25519" && sleep 1
+
